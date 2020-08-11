30f2b70885b8376fe261d15061a90b1234a2ad38
Aleksandar Picuric <apicuric@gmail.com>
2020-08-11 15:07:01 +0200

    Enable spread operator
    Enabling spread operator for this project with cmd npm install --save-dev babel-preset-stage-2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-
-  <p>{{msg}}</p>
+<!-- 1. kako spread operator nije pokriven (...) dodacemo ga preko npm install --save-dev babel-preset-stage-2
+    takodje je potrebo u .bablerc dodati jos jedan red ["stage-2"] -->
+  <div class="container">
+    <p>{{msg}}</p>
+  </div>
 </template>
 
 <script>
