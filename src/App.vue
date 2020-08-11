d2fee1101d028246398692ea43e435a91a0eeefe
Aleksandar Picuric <apicuric@gmail.com>
2020-08-11 22:22:07 +0200

    Header component finnished
    Navigation with dropdown added to the project

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,31 @@
 <!-- 1. kako spread operator nije pokriven (...) dodacemo ga preko npm install --save-dev babel-preset-stage-2
     takodje je potrebo u .bablerc dodati jos jedan red ["stage-2"] -->
   <div class="container">
-    <!-- 10. sada cemo preko router-view reci nasim rutama gde da se ucitaju -->
-    <router-view></router-view>
+    <app-header-component></app-header-component>
+      <!-- 10. sada cemo preko router-view reci nasim rutama gde da se ucitaju -->
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
 export default {
   data () {
     return {
     }
+  },
+  components: {
+    'app-header-component': HeaderComponent
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  body{
+    padding: 30px;
+  }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>The Header component</h1>
+
+  <!-- <ul class="nav nav-pills"> -->
+    <!-- 11. preko router-linka kreiramo linkove koji ce nas voditi ka odredjenim rutama, idemo da registrujemo
+          nasu headaer cmpt u app.vue -->
+    <!-- <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/portfolio" tag="li" active-class="active"><a>Portfolio</a></router-link>
+    <router-link to="/stocks" tag="li" active-class="active"><a>Stocks</a></router-link>
+  </ul> --> 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <router-link to="/" class="navbar-brand" active-class="active" exact>Home</router-link>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <router-link to="/portfolio" tag="li" active-class="active"><a>Portfolio</a></router-link>
+        <router-link to="/stocks" tag="li" active-class="active"><a>Stocks</a></router-link>
+      </ul>
+      <span class="navbar-text navbar-right">
+        Funds: 0
+      </span>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="nav-item"><a href="#">End Day</a></li>
+        <li class="nav-item dropdown">
+        <a 
+          class="nav-link dropdown-toggle" 
+          href="#" id="navbarDropdown" 
+          role="button" 
+          data-toggle="dropdown" 
+          aria-haspopup="true" 
+          aria-expanded="false">
+          Save & Load
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Save Data</a>
+          <a class="dropdown-item" href="#">Load Data</a>
+        </div>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
 </template>
