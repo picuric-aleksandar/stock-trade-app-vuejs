f30f88a1fa41f1f6d70e2bc3e39ba1997f0b0895
Aleksandar Picuric <apicuric@gmail.com>
2020-08-23 19:45:31 +0200

    Stock cmpt update 1.1
    Implementation of different single stock layout based on query param from route.
    Query param is either 'stock' or 'portfolio'

--- src/components/stocks/StockComponent.vue
+++ src/components/stocks/StockComponent.vue
@@ -1,17 +1,53 @@
 <template>
   <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
+    <div 
+      v-if="$route.query.view == 'stocks'"
+      class="panel panel-success">
       <div class="panel-heading">
-          <h3 class="panel-title">Name
-            <small>(Price: 0)</small>
+          <h3 class="panel-title">{{stock.name}}
+            <small>(Price: {{stock.price}})</small>
           </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
-          <input type="text" name="quantity" placeholder="Quantity" class="form-control">
+          <input 
+            type="number" 
+            placeholder="Quantity" 
+            class="form-control"
+            v-model="quantity">
         </div>
         <div class="pull-right">
-          <div class="btn btn-success">
+          <div 
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled = "quantity <= 0 || !Number.isInteger(Number(quantity))"> <!--ako je tip inputa number dovoljno je !Number.isInteger(quantity)-->
+            Buy
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="$route.query.view == 'portfolio'"
+      class="panel panel-info">
+      <div class="panel-heading">
+          <h3 class="panel-title">{{stock.name}}
+            <small>(Price: {{stock.price}})</small>
+            <small>Quantity: </small>
+          </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+            type="number" 
+            placeholder="Quantity" 
+            class="form-control"
+            v-model="quantity">
+        </div>
+        <div class="pull-right">
+          <div 
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled = "quantity <= 0 || !Number.isInteger(Number(quantity))"> <!--ako je tip inputa number dovoljno je !Number.isInteger(quantity)-->
             Buy
           </div>
         </div>
@@ -20,7 +56,43 @@
   </div>
 </template>
 
-
+<script>
+import * as types from '../../store/types';
+import {mapActions} from 'vuex';
+export default {
+  props: ['stock'],
+  data(){
+    return {
+      quantity: 0,
+    }
+  },
+  watch: {
+    quantity(){
+      console.log(this.quantity)
+    }
+  },
+  methods: {
+    ...mapActions({
+      buy: types.BUY_STOCKS
+    }),
+    buyStock(){
+      if(this.quantity <= 0){
+        //umesto alerta,jednostavno cemo disable-ovati dugme
+        return alert('Quantity must be greater than 0 if you want to buy this stock!')
+      }
+      //kupovina deonice ce biti nas order stoga cemo kreirati konstantu koja to predstavlja
+      const stockOrder  = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        stockQuantity: this.quantity
+      }
+      console.log(stockOrder);
+      this.buy(stockOrder);
+      this.quantity = 0;
+    }
+  }
+}
+</script>
 
 
 <style scoped>
